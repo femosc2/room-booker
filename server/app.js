c3669b84d806f7cbf75bef020dae58869327e376
Felix Morau <felixmorau@gmail.com>
2019-01-02 07:01:01 +0100

    server works, all that is left is css and html

--- server/app.js
+++ server/app.js
@@ -1,53 +1,66 @@
 const axios = require("axios")
 
-const bookedTimeslots = {
-    "data" : [{
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "00.00-02.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "02.00-04.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "04.00-06.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "06.00-08.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "08.00-10.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "10.00-12.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "12.00-14.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "14.00-16.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "18.00-20.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "20.00-22.00"
-    }, {
-      "booked" : false,
-      "bookedBy" : "asd",
-      "time" : "22.00-00.00"
-    } ]
-  }
-  
+const bookedTimeslots = [{
+        time: "00.00-02.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "02.00-04.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "04.00-06.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "06.00-08.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "08.00-10.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "10.00-12.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "12.00-14.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "14.00-16.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "16.00-18.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "18.00-20.00",
+        booked: false,
+        bookedBy: ""
+    },
+    {
+        time: "20.00-22.00",
+        booked: false,
+        bookedBy: null
+    },
+    {
+        time: "22.00-00.00",
+        booked: false,
+        bookedBy: ""
+    }
+];
 
 axios.delete("https://room-booker-37ff4.firebaseio.com/data.json")
     .then(
@@ -61,7 +74,7 @@ axios.delete("https://room-booker-37ff4.firebaseio.com/data.json")
     
 setTimeout(function() {
     for (var i = 0; i < bookedTimeslots.length; i++) {
-        axios.post("https://room-booker-37ff4.firebaseio.com/data.json", bookedTimeslots[i])
+        axios.put("https://room-booker-37ff4.firebaseio.com/data/" + i + ".json", bookedTimeslots[i])
     .then(
         response => {
             console.log(response);
